8d3cc5853fdc77f42947f747a14a573b1804112f
dnlfmkn <famakindaniel7@gmail.com>
2020-03-21 09:19:43 -0400

    added bang-bang barebones, need to add logic for determining obstacle-free path

--- control.asm
+++ control.asm
@@ -24,6 +24,10 @@ TRISB	EQU    0x86
 TRISA   EQU    0x85  
 PIE1	EQU    0x8C
 ADCON0  EQU    0x1F
+ADCON1  EQU    0x9F
+PIR1    EQU    0x0C
+ADRESH  EQU    0x1E
+ADRESL	EQU    0x9E  
 
 ;-------------------------------------------------------------------------------
 ;			    USER-DEFINED PORT ALIASES
@@ -34,7 +38,9 @@ IN2	EQU    0x05
 EN3_4   EQU    0x04
 IN3	EQU    0x03
 IN4     EQU    0x02
-ADIE	EQU    0x06		    ; 6th bit of PIE1 interrupt register     
+ADIE	EQU    0x06		    ; 6th bit of PIE1 register 
+ADIF    EQU    0x06		    ; 6th bit of PIR1 register
+GO	EQU    0x02		    ; 2nd bit of ADCON0    
      
 ALL_FWD_LED	   EQU    0x00	    ; red PORTD pins
 ALL_REV_LED	   EQU    0x01	    ; green
@@ -54,6 +60,9 @@ LFT_TRN_REV_LED	   EQU    0x05	    ; red again because LED colors are limited
 		       Kount10ms
 		       Kount1s
 		       Kount5
+		       ADC_RES			   ; ADC result
+		       BANG_BANG_THRESH		   ; distance threshold for bang-bang
+		       SHOULD_MOVE
 		ENDC
 		
 ;--------------------------------------------------------------------------
@@ -66,18 +75,6 @@ LFT_TRN_REV_LED	   EQU    0x05	    ; red again because LED colors are limited
 		nop
 		nop
 		nop	
-		
-;--------------------------------------------------------------------------
-;			  SENSOR LOGIC
-;--------------------------------------------------------------------------
-;**************************************************************************
-; Reference: [http://www.mwftr.com/ucF08/LEC15%20PIC%20AD.pdf]
-;		
-; The first step is to configure the A/D logic. [See the `start` label]
-; These values will be converted to their digital equivalent by the ADC module
-; of the PIC.
-;****************************************************************************
-		
 		
 ;--------------------------------------------------------------------------
 ;			  ALL-FORWARD LOGIC
@@ -206,6 +203,61 @@ impl_1s
 		nop
 		return
 		
+;***************************************************************************
+;			  SENSOR LOGIC
+;***************************************************************************
+; Reference: [http://www.mwftr.com/ucF08/LEC15%20PIC%20AD.pdf]
+;		
+; The first step is to configure the A/D logic. [See the `init` label]
+;
+; Afterwards, the `adc` routine (below) is called which converts values from the
+; sensor and stores the result in the ADRESH register (last 8 bits) for further
+; use.
+;
+;-------------------------------------------------------------------------------
+; NOTE: Since we are restricted to 8 bits, our resolution is from 0-255 max
+;
+; So, the conversion from adc value to voltage is like so:
+;   [analog_voltage] = 5/255 * [adc_val] --> where 5V is maximum range of ADC
+;-------------------------------------------------------------------------------
+;
+; For now, we're implementing Bang-Bang, so once, we hit a distance threshold,
+; we stop the vehicle and find free directions to turn to. 
+; We'll have this distance threshold be [24 cm] based on experiments. 
+;
+; In the future, the voltage obtained from the ADC is fed into an equation
+; chosen based on experiments run with the IR sensor. The resulting value is the 
+; distance from the obstacle. This distance will then be used to calculate the  
+; speed at which the vehicle should move to avoid hitting the obstacle.
+; 
+; For now the equation will be 13*(analog_voltage)^-1
+;
+; This is *PID control.*
+;-------------------------------------------------------------------------------
+		
+test_distance_thresh	movlw  ADC_RES
+			subwf  BANG_BANG_THRESH
+			skpnc			; if carry bit is clear, then ADC result > threshold
+			nop
+			clrf   SHOULD_MOVE	; clear the signal to keep moving
+			return
+		
+;-------------------------------------------------------------------------------
+		
+;-------------------------------------------------------------------------------
+;			    ADC CONVERSION
+;-------------------------------------------------------------------------------
+		
+adc		call    delay10ms
+		banksel	ADCON0
+		bsf	ADCON0, GO	; triggers the AD conversion
+ 
+adcloop         btfsc   ADCON0, GO	; wait for conversion to be done
+		goto	adcloop
+		bcf	PIR1, ADIF	; clear conversion finished bit
+		movf	ADRESH, 0	; writes result from ADRESH to W register
+		return	
+		
 ;--------------------------------------------------------------------------
 ;				MAIN
 ;--------------------------------------------------------------------------
@@ -235,9 +287,29 @@ init		    movlw	0xFF	      ; initializing A/D module
 		    banksel     ADCON1	      ; ADC result configuration 
 		    movlw       0x80	      ; this byte sets the result as
 		    movwf       ADCON1	      ; right-justified in ADRESH-ADRESL
-		
+		    
+		    banksel     BANG_BANG_THRESH
+		    movlw	0x1E	      ; magic
+		    movwf	BANG_BANG_THRESH
 		    movlw	0x05
 		    movwf	Kount5
+
+loop		    banksel     PIR1	      ; PIR1 register contains result of A/D conversion
+		    bcf		PIR1, ADIF    ; clear ADIF bit to start the next conversion
+		    banksel     ADC_RES
+		    clrf	ADC_RES	      ; clear conversion result for next run of ADC
+		    call        adc	      	
+		    banksel	ADC_RES
+			movwf	ADC_RES	           ; after `adc` routine runs, ADC result will be
+						   ; written to W register which we can access
+						   ; and use to determine the next course of
+						   ; action
+		    call	test_distance_thresh
+test_keep_moving    andwf	SHOULD_MOVE        ; test if should_move is zero
+		    skpz
+		    nop
+		    call	determine_next_direction
+		    goto	loop
 		
 all_fwd_test	    call	all_forward
 		    bsf		PORTD, ALL_FWD_LED 
